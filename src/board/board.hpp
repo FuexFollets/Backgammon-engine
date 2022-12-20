e656cb80715e0242191f3992daa0b442d4c33586
FuexFollets <fuex9follets@gmail.com>
2022-12-19 23:48:54 -0500

    Added initializers for move type

--- src/board/board.hpp
+++ src/board/board.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <vector>
 
 namespace board {
     using point = signed char;
@@ -11,17 +12,22 @@ namespace board {
         std::array<point, 24> position;
         point bar;
         point white_finish, black_finish;
-
         bool turn; // 0 = white, 1 = black
 
         struct complete_move {
             struct individual_move {
+                individual_move() {}
+                individual_move(point p); // Bar move
+                individual_move(point p_start, point p_end); // Normal move
+
                 point start, end;
 
-                bool move_type(); // false = normal move, true == moving off the bar
+                bool move_type(); // false = normal move, true = moving off the bar
             };
 
+            complete_move(const std::initializer_list<individual_move> moves) : complete_move_composition {moves} {}
 
+            std::vector<individual_move> complete_move_composition;
         };
 
         void make_move(const complete_move&);
