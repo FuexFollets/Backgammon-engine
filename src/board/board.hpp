6b24c856fcf483d0a1a559239d8fcec8dda66523
FuexFollets <fuex9follets@gmail.com>
2022-12-19 23:50:46 -0500

    Made the default board position constexpr static

--- src/board/board.hpp
+++ src/board/board.hpp
@@ -6,7 +6,7 @@
 namespace board {
     using point = signed char;
 
-    const std::array<point, 24> default_position {2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2};
+    static constexpr const std::array<point, 24> default_position {2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2};
 
     struct board {
         std::array<point, 24> position;
